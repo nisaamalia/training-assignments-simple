7bf24f014bf974c98addeb88826e4baab5ab8a2f
nisaamalia <amallianissa@gmail.com>
2019-04-15 10:47:29 +0700

    write code once

--- src/java/eu/sig/training/ch04/SavingsAccount.java
+++ src/java/eu/sig/training/ch04/SavingsAccount.java
@@ -1,43 +1,17 @@
 package eu.sig.training.ch04;
 
 // tag Savings Account
-public class SavingsAccount {
-    private static final float INTEREST_PERCENTAGE = 0.04f;
-    private Money balance = new Money();
+public class SavingsAccount extends Account {
     private CheckingAccount registeredCounterAccount;
 
     public Transfer makeTransfer(String counterAccount, Money amount) 
         throws BusinessException {
-        // 1. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0; // <1>
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        if (sum % 11 == 0) {
-            // 2. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount); // <2>
-            // 3. Check whether withdrawal is to registered counter account:
-            if (result.getCounterAccount().equals(this.registeredCounterAccount)) 
-            {
-                return result;
-            } else {
-                throw new BusinessException("Counter-account not registered!");
-            }
+        Transfer result = super.makeTransfer(counterAccount, amount);
+        if (result.getCounterAccount().equals(this.registeredCounterAccount)) {
+        	return result;
         } else {
-            throw new BusinessException("Invalid account number!!");
+        	throw new BusinessException("Counter-account not registered");
         }
-    }
-
-    public void addInterest() {
-        Money interest = balance.multiply(INTEREST_PERCENTAGE);
-        if (interest.greaterThan(0)) {
-            balance.add(interest);
-        } else {
-            balance.substract(interest);
-        }
-    }
+   	}
 }
 // end Savings Account
